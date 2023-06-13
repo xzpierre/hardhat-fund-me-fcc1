08a00d1917d6f5ff8f0f5f2c88640081ddbe0763
xzpierre <goatpp@gmail.com>
2023-06-13 11:22:06 +0800

    your commit message

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,23 @@
-require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("@nomiclabs/hardhat-etherscan")
-require("dotenv").config()
-require("solidity-coverage")
-require("hardhat-deploy")
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("solidity-coverage");
+require("hardhat-deploy");
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 const SEPOLIA_RPC_URL =
     process.env.SEPOLIA_RPC_URL ||
-    "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+    "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY";
 const PRIVATE_KEY =
     process.env.PRIVATE_KEY ||
-    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -52,7 +52,7 @@ module.exports = {
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
-        // coinmarketcap: COINMARKETCAP_API_KEY,
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
     namedAccounts: {
         deployer: {
@@ -63,4 +63,4 @@ module.exports = {
     mocha: {
         timeout: 500000,
     },
-}
+};
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -7,11 +7,11 @@ const networkConfig = {
         name: "sepolia",
         ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
     },
-}
+};
 
-const developmentChains = ["hardhat", "localhost"]
+const developmentChains = ["hardhat", "localhost"];
 
 module.exports = {
     networkConfig,
     developmentChains,
-}
+};
